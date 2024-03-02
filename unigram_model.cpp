aa5de903b1ed1dffa72520dd50cb370e6b9d64ad
hetricke <hetricke@umich.edu>
2024-03-01 20:56:39 -0500

    finshed c++ getWordCode method and started work on the updateDictionary method

--- unigram_model.cpp
+++ unigram_model.cpp
@@ -1,6 +1,8 @@
 //
 // Created by hetricke on 3/1/24.
 //
+#include <iostream>
+#include <fstream>
 using namespace std;
 
 struct Unigram{
@@ -42,11 +44,78 @@ void updateAlphabetCount(string letter, int count){
 }
 
 int getWordCode(string word){
+    string file_name = word.substr(0,1) + "_dictionary.txt";
+    ifstream dictionary_file;
+    dictionary_file.open(file_name);
 
-    return 0;
+    int start = 0;
+
+    auto search_result = lower_bound(alphabet.begin(), alphabet.end(), word.substr(0,1));
+    int end = distance(alphabet.begin(), search_result);
+
+    int mid = (start+end)/2;
+
+    while(start<=end){
+        string file_word;
+        getline(dictionary_file, mid)
+        if(file_word == word){
+            dictionary_file.close()
+            return mid;
+        }
+        else if (file_word < word){
+            start = mid+1;
+        }
+        else{
+            end = mid-1;
+        }
+        mid = (start+end)/2;
+
+    }
+
+    dictionary_file.close()
+
+    return -1;
 }
 
 void updateDictionary(string word){
+    string file_name = word.substr(0,1) + "_dictionary.txt";
+    ifstream dictionary_file;
+    dictionary_file.open(file_name);
+
+    int start = 0;
+
+    auto search_result = lower_bound(alphabet.begin(), alphabet.end(), word.substr(0,1));
+    int end = distance(alphabet.begin(), search_result);
+
+    int mid = (start+end)/2;
+
+    while(start<=end){
+        string file_word;
+        getline(dictionary_file, mid)
+        if(file_word == word){
+            dictionary_file.close()
+            return;
+        }
+
+        //TODO: check if this will exceed the end of the file
+        //TODO: read in all the file after this point into a vector and make the changes to the txt file
+        string next_file_word;
+        getline(dictionary_file, mid+1)
+
+        if ()
+        else if (file_word < word){
+            start = mid+1;
+        }
+        else{
+            end = mid-1;
+        }
+        mid = (start+end)/2;
+
+    }
+
+    dictionary_file.close()
+
+    return -1;
 
     return;
 }
